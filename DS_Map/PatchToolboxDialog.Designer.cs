9c453c4a6721887a2643a63f76fb7052c817cf36
Dennis Fedotov <contact@kalaay.dev>
2026-01-26 17:15:17 +0100

    fix wrong length message for #31, still needs documentation for battle frontier messages

--- DS_Map/PatchToolboxDialog.Designer.cs
+++ DS_Map/PatchToolboxDialog.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace DSPRE
+namespace DSPRE
 {
     partial class PatchToolboxDialog
     {
@@ -506,7 +506,7 @@
             this.expandTrainerNamesTextLBL.Name = "expandTrainerNamesTextLBL";
             this.expandTrainerNamesTextLBL.Size = new System.Drawing.Size(288, 58);
             this.expandTrainerNamesTextLBL.TabIndex = 42;
-            this.expandTrainerNamesTextLBL.Text = "Expands trainer names to be up to 16 characters long.";
+            this.expandTrainerNamesTextLBL.Text = "Expands trainer names to be up to 12 characters long.";
             this.expandTrainerNamesTextLBL.UseMnemonic = false;
             // 
             // expandTrainerNamesLBL
